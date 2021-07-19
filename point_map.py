7b1b26ae51d6b200f4ee794639696504f0be74b8
prof401 <gerald.scheetz@april1.net>
2021-07-18 23:30:52 -0600

    single point for downloaded game works

--- point_map.py
+++ point_map.py
@@ -5,20 +5,22 @@ from mplsoccer import VerticalPitch
 
 def show_shots(shot_data):
     pitch = VerticalPitch(pitch_color='black', line_color='white', half=True,
-                          pitch_type="custom", pitch_width=62, pitch_length=120,
+                          pitch_type="custom", pitch_width=56, pitch_length=120,
                           constrained_layout=True, tight_layout=False, goal_type='box',
                           axis=True, label=True)
     fig, ax = pitch.draw()
-    plt.show()
-    yd_to_m=0.9144
-    x_slope=16.9
-    x_int=-32.9
-    y_slope=-13
-    y_int=858.3571273
 
-    x_array=[]
-    y_array=[]
+
+    x_slope=1024/61
+    x_int=0
+    y_slope=-13
+    y_int=874
+    yd_to_m=0.9144
+
     for shot in shot_data:
-        x_array.append((shot['field_x']-x_int)/x_slope*yd_to_m)
-        y_array.append(120-((shot['field_y']-y_int)/y_slope*yd_to_m))
-    pitch.scatter(x_array, y_array,ax)
+        x_plot=((shot['field_x']-x_int)/x_slope*yd_to_m)
+        y_plot=(120-((shot['field_y']-y_int)/y_slope*yd_to_m))
+        ax.plot(x_plot,y_plot,'ro')
+        print(x_plot,y_plot)
+
+    plt.show()
--- shot_map.py
+++ shot_map.py
@@ -2,8 +2,8 @@ import get_single_file_shots as gs
 import point_map as pm
 
 #shot_list = gs.file_shot_data("VidSwap-Data-Export-Playlist.json")
-#shot_list = gs.file_shot_data("VidSwap-Data-Export-Playlist-464721.json")
-shot_list = gs.file_shot_data("VidSwap-edges.json")
+shot_list = gs.file_shot_data("VidSwap-Data-Export-Playlist-464721.json")
+#shot_list = gs.file_shot_data("VidSwap-edges.json")
 
 for shot in shot_list:
     print(shot)
